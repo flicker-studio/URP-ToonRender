551b421fa6aa18b64d7a43fe46be4ff155716fba
Zzz <Morsiusiurandum@outlook.com>
2024-07-05 21:04:25 +0800

    feat: finish transparent shader

--- render-dev/Assets/Non-Photorealistic RP/Tests/MaterialProperties.cs
+++ render-dev/Assets/Non-Photorealistic RP/Tests/MaterialProperties.cs
@@ -10,13 +10,13 @@ namespace Non_Photorealistic_RP.Tests
     {
         private static readonly int BaseColorId = Shader.PropertyToID("_BaseColor");
 
-        [SerializeField] private Color baseColor = Color.white;
-
         private static MaterialPropertyBlock _block;
+
+        [SerializeField] private Color baseColor = Color.white;
 
         private void Awake()
         {
-            baseColor = new Color(Random.value, Random.value, Random.value);
+            baseColor = new Color(Random.value, Random.value, Random.value, 0.5f);
             OnValidate();
         }
 
